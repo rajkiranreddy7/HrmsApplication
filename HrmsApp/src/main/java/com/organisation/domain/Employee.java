67af502c11fd0b231293b1549b47df48b2ab35e6
rajkiranreddy7 <rajkiranreddy7@gmail.com>
2024-04-14 14:35:14 +0530

    Added employee details class

--- HrmsApp/src/main/java/com/organisation/domain/Employee.java
+++ HrmsApp/src/main/java/com/organisation/domain/Employee.java
@@ -1,10 +1,21 @@
 package com.organisation.domain;
 
+import java.util.List;
+
 public class Employee {
 	
 	private long employeeId;
 	private String employeeName;
 	private String companyName;
+	private String managerName;
+	
+	public List<EmployeeDetails> getEmployeeDetailsList() {
+		return employeeDetailsList;
+	}
+	public void setEmployeeDetailsList(List<EmployeeDetails> employeeDetailsList) {
+		this.employeeDetailsList = employeeDetailsList;
+	}
+	private List<EmployeeDetails> employeeDetailsList;
 
 	public long getEmployeeId() {
 		return employeeId;
@@ -23,6 +34,13 @@ public class Employee {
 	}
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
+	}
+	
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
 	}
 
 }
---
+++ HrmsApp/src/main/java/com/organisation/domain/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package com.organisation.domain;
+
+public class EmployeeDetails {
+	
+	private int year;
+	private long salary;
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	
+
+}
