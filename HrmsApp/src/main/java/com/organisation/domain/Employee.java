e27aeef8f588035a6dc650584916661b702f8b86
rajkiranreddy7 <rajkiranreddy7@gmail.com>
2024-04-14 14:27:44 +0530

    Added employee class

---
+++ HrmsApp/src/main/java/com/organisation/domain/Employee.java
@@ -0,0 +1,28 @@
+package com.organisation.domain;
+
+public class Employee {
+	
+	private long employeeId;
+	private String employeeName;
+	private String companyName;
+
+	public long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+}
